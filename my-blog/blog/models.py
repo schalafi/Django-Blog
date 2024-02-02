399e2da1972e23f6d44b65ec4cd68e75de928ce6
schalafi <118856089+schalafi@users.noreply.github.com>
2024-02-02 03:18:03 +0000

    Add Post and Category models

--- my-blog/blog/models.py
+++ my-blog/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+# Category model
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+# Post model
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add = True)
+    last_modified = models.DateTimeField(auto_now_add = True)
+    categories = models.ManyToManyField("Category", related_name = "posts")
+
+    
